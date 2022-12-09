f721e65271dc0668820d2c1cd0cd6c9312240ee5
GreenBeansAgainstVeganism <kradrling@gmail.com>
2022-12-08 23:48:26 -0800

    slickened photo carousel

--- script.js
+++ script.js
@@ -12,9 +12,9 @@ $(function(){
 		// Event that fires when an element is clicked
 		let setIndex = function(n)
 		{
+			n = Math.max(Math.min(n,items.length-1),0);
 			console.log(curIndex,n);
-			curIndex = n;
-			items.forEach((item,index) => {
+			/* if(curIndex != n)  */items.forEach((item,index) => {
 				let x = index-n;
 				let heightPercent = 100-Math.abs(x)*10;
 				console.log(item.naturalWidth,item.naturalHeight,item.parentElement.clientHeight,heightPercent);
@@ -26,6 +26,7 @@ $(function(){
 					// opacity: 1-Math.abs(x)*0.3
 				},{duration:160,queue:false,always:()=>{item.style.filter = `brightness(${Math.max(20,100-Math.abs(x)*40)}%)`;}});
 			});
+			curIndex = n;
 		}
 		
 		items.forEach((item,index) => {
@@ -37,5 +38,19 @@ $(function(){
 			item.style.filter = `brightness(${Math.max(20,100-index*40)}%)`;
 			$(item).click(()=>setIndex(index));
 		});
+		
+		let controldiv = $('<div class="carousel-controls"></div>');
+		$(e).append(controldiv);
+		
+		// create back button
+		$(controldiv).append($('<button type="button" class="carousel-button carousel-button-left"></button>').append($('<img src="assets/carousel_arrow.svg">').css('transform','scaleX(-1)')).click(()=>setIndex(curIndex-1)));
+		
+		// create forward button
+		$(controldiv).append($('<button type="button" class="carousel-button carousel-button-right"></button>').append($('<img src="assets/carousel_arrow.svg">')).click(()=>setIndex(curIndex+1)));
+		
+		// create dots
+		$(controldiv).append($('<div class="carousel-dots"></div>').append(...items.map((item,index) => {
+			return $('<button type="button" class="carousel-dot"></button>').append($('<span></span>')).click(()=>setIndex(index));
+		})));
 	});
 });
